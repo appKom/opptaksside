4f9688bc89a168dde7483fba3e3ac33dae6ee632
Julian Ammouche Ottosen <42567826+julian-ao@users.noreply.github.com>
2026-01-20 13:16:18 +0100

    ..

--- components/CommitteeAboutCard.tsx
+++ components/CommitteeAboutCard.tsx
@@ -11,42 +11,44 @@ const CommitteeAboutCard = ({
   committee,
   hasPeriod,
   isInterviewing,
-}: CommitteeAboutCardProps) => (
-  <div>
-    <img
-      src={committee.imageUrl || "/Online_svart_o.svg"}
-      alt={committee.name}
-      className="w-16 h-16 p-1 mb-2 bg-white rounded-full md:w-24 md:h-24"
-    />
-
-    <div className="flex flex-col items-start gap-2 mb-1 md:flex-row md:items-center md:mb-0">
-      <h3 className="text-xl font-bold dark:text-white">
-        {committee.name}
-        {committee.name !== committee.abbreviation &&
-          `(${committee.abbreviation})`}
-      </h3>
-      {hasPeriod && (
-        <span className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300 whitespace-nowrap">
-          Har opptak!
-        </span>
-      )}
-      {isInterviewing && !hasPeriod && (
-        <span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300 whitespace-nowrap">
-          Intervjuer pågår
-        </span>
-      )}
+}: CommitteeAboutCardProps) => {
+  return (
+    <div>
+      <img
+        src={committee.imageUrl || "/Online_svart_o.svg"}
+        alt={committee.name}
+        className="w-16 h-16 p-1 mb-2 bg-white rounded-full md:w-24 md:h-24"
+      />
+  
+      <div className="flex flex-col items-start gap-2 mb-1 md:flex-row md:items-center md:mb-0">
+        <h3 className="text-xl font-bold dark:text-white">
+          {committee.name}
+          {committee.name !== committee.abbreviation &&
+            `(${committee.abbreviation})`}
+        </h3>
+        {hasPeriod && (
+          <span className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300 whitespace-nowrap">
+            Har opptak!
+          </span>
+        )}
+        {isInterviewing && !hasPeriod && (
+          <span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300 whitespace-nowrap">
+            Intervjuer pågår
+          </span>
+        )}
+      </div>
+      <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+        {committee.email}
+      </p>
+      <div
+        className="text-gray-500 whitespace-pre-wrap dark:text-gray-400 [&_a]:text-blue-600 [&_a]:underline [&_a]:hover:text-blue-800 dark:[&_a]:text-blue-400 dark:[&_a]:hover:text-blue-300
+        "
+        dangerouslySetInnerHTML={{ __html: sanitizeCommitteeDescription(committee.description || "Ingen beskrivelse :(") }}
+      // dangerouslySetInnerHTML allows for rendering HTML content like images and links
+      />
     </div>
-    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-      {committee.email}
-    </p>
-    <div
-      className="text-gray-500 whitespace-pre-wrap dark:text-gray-400 [&_a]:text-blue-600 [&_a]:underline [&_a]:hover:text-blue-800 dark:[&_a]:text-blue-400 dark:[&_a]:hover:text-blue-300
-      "
-      dangerouslySetInnerHTML={{ __html: sanitizeCommitteeDescription(committee.description || "Ingen beskrivelse :(") }}
-    // dangerouslySetInnerHTML allows for rendering HTML content like images and links
-    />
-  </div>
-);
+  );
+};
 
 export default CommitteeAboutCard;
 
